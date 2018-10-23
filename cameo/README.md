e194cf53557e9825e197928e81f77b05d17766cb
2niuhe <18362903329nh@gmail.com>
2018-10-23 11:20:40 +0800

    init

---
+++ cameo/README.md
@@ -0,0 +1,12 @@
+###Cameo
+
+All code is from the book 《Learning OpenCV 3 Computer Vision with Python》second Edition.
+
+run on ubutnu16.0/python3.x/opencv3.x
+
+####Function:
+
+- capture camera's frame
+- filter the frame
+- show the result
+- screenshot/screencast
---
+++ cameo/cameo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import cv2
+from managers import WindowManager,CaptureManager
+import filters
+
+class Cameo(object):
+    def __init__(self):
+        self._windowManager = WindowManager('Cameo',self.onKeypress)
+        self._captureManager = CaptureManager(cv2.VideoCapture("/dev/video0"),self._windowManager,True)
+        self._curveFilter = filters.EmbossFilter()
+
+    def run(self):
+        """run the main loop"""
+        self._windowManager.createWindow()
+        while self._windowManager.isWindowCreated:
+            self._captureManager.enterFrame()
+            frame = self._captureManager.frame
+            # TODO:Filter the framea
+            filters.strokeEdges(frame,frame)
+            self._curveFilter.apply(frame,frame)
+            self._captureManager.exitFrame()
+            self._windowManager.processEvents()
+
+    def onKeypress(self,keycode):
+        """
+        Handle a keypress
+
+        space   ->  Take a screenshot.
+        tab     ->  Start/Stop video record
+        escape  ->  Quit
+
+        """
+        if keycode == 32:   #space
+            self._captureManager.writeImage('screenshot.png')
+        elif keycode == 9:  #tab
+            if not self._captureManager.isWritingVideo:
+                self._captureManager.startWritingVideo('screencast.avi')
+            else:
+                self._captureManager.stopWritingVideo()
+        elif keycode == 27: #quit
+            self._windowManager.destoryWindow()
+
+if __name__ == "__main__":
+    Cameo().run()
